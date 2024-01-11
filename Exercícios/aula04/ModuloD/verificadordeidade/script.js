d2f2cd73a64c2c78bd6a79a3d983ad54e5393c2d
Luiza <ludpaiva@gmail.com>
2024-01-11 09:33:29 -0300

    Idade e genero
    Verifica idade e genero

--- Exercícios/aula04/ModuloD/verificadordeidade/script.js
+++ Exercícios/aula04/ModuloD/verificadordeidade/script.js
@@ -1,15 +1,30 @@
 function verificar(){
     
+    // Declara resultado
+    var resultado = window.document.getElementById('resultado')
 
     //Ano atual
     var data = new Date()
     var anoatual = data.getFullYear()
     
-    // Calculando idade
-    var ano = window.document.getElementById('ano')
-    var idade = anoatual - ano;
+    // Calcula idade
+    var ano = document.getElementById('ano')
+    var idade = anoatual - Number(ano.value)
 
-    // Mostrando idade
-    var resultado = window.document.getElementById('resultado')
-    resultado.innerHTML = (`Tem ${idade} anos de idade`) 
+    // Verifica ano entrada
+    if (ano.value.length == 0 || ano.value > anoatual) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else { 
+    
+        // Verifica gênero e idade 
+    var genero = document.getElementsByName('genero')
+    var gen = ''
+    if (genero[0].checked) {
+        gen = 'Mulher'
+    } else {
+        gen = 'Homem'
+    }
+    
+    resultado.innerHTML = (`${gen} com ${idade} anos de idade.`) 
+    }
 }
