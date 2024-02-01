a63a6a59b9b3078fe523fea3c90be6360539973d
Luiza de Paiva <136637348+luizadepaiva@users.noreply.github.com>
2024-02-01 18:13:43 -0300

    Alterações LiveServer

--- Exercícios/aula04/ModuloD/contador/script.js
+++ Exercícios/aula04/ModuloD/contador/script.js
@@ -1,13 +1,13 @@
+function contar () {
+    
 // Variáveis
 var entrada = window.document.getElementById('inicio')
 var final = window.document.getElementById('fim')
 var incremento = window.document.getElementById('passo')
 var mensagem = window.document.getElementById('resultado')
+
+mensagem.innerHTML = (`Valor de entrada é ${entrada}, valor final é ${fim} e o valor do passo é ${incremento}`)
+
 // Condições
 
-if (incremento==0){
-    window.alert('Valor de passo inserido = 0. Consirando passo = 1')
-} else if{
-    mensagem.innerHTML = `Valor de entrada é ${entrada}, valor final é ${fim} e o valor do passo é ${incremento}`
 }
-
