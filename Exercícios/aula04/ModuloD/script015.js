e1ac801e2a019e816b45f470256552af9d568af2
Luiza de Paiva <136637348+luizadepaiva@users.noreply.github.com>
2024-02-01 15:26:43 -0300

    Contador

---
+++ Exercícios/aula04/ModuloD/script015.js
@@ -0,0 +1,8 @@
+function contar() {
+    console.log('Vai começar')
+    for (var contador=1; contador<=6; contador++){
+        console.log(contador)
+        }
+        console.log('Fim!')
+}
+
---
+++ Exercícios/aula04/ModuloD/script15.js
@@ -0,0 +1,29 @@
+var contador = 1
+
+//While
+while (contador < 6) {
+    console.log('Tudo bem?')
+    contador++
+}
+
+// Do While
+
+do {
+    contador++
+} while (contador<6)
+
+// For
+for (var contador=1; contador<=6;contador++){
+    console.log(contador)
+}
+
+// For
+console.log('Vai começar')
+for (var contador=1; contador<=6; contador++){
+    console.log(contador)
+    }
+    console.log('Fim!')
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+
