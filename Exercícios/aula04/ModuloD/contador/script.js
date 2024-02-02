6a4df7ba9a179e0523308f58cb6da7d0b5bd9bed
Luiza de Paiva <136637348+luizadepaiva@users.noreply.github.com>
2024-02-02 11:48:14 -0300

    Condicionais

--- Exercícios/aula04/ModuloD/contador/script.js
+++ Exercícios/aula04/ModuloD/contador/script.js
@@ -1,13 +1,35 @@
-function contar () {
-    
-// Variáveis
-var entrada = window.document.getElementById('inicio')
-var final = window.document.getElementById('fim')
-var incremento = window.document.getElementById('passo')
-var mensagem = window.document.getElementById('resultado')
+function contar() {
 
-mensagem.innerHTML = (`Valor de entrada é ${entrada}, valor final é ${fim} e o valor do passo é ${incremento}`)
+    // Variáveis
+    let entrada = window.document.getElementById('inicio')
+    let final = window.document.getElementById('fim')
+    let incremento = window.document.getElementById('passo')
+    let mensagem = window.document.getElementById('resultado')
 
-// Condições
+    let ent = Number(entrada.value)
+    let fin = Number(final.value)
+    let inc = Number(incremento.value)
+
+    // Verificando valores de entrada
+    if (entrada.value.length == 0 || final.value.length == 0 || incremento.value.length == 0) {
+        mensagem.innerHTML = 'Impossível contar'
+    } else {
+        mensagem.innerHTML = 'Contando: <br>'
+        if (inc <= 0)
+            window.alert('ERRO! Passo inválido. Considerando passo = 1')
+            inc = 1
+        if (ent < fin) {
+            // Contagem crescente
+            for (let c = ent; c <= fin; c += inc) {
+                mensagem.innerHTML += `${c} \u{1F449}`
+            }
+        } else { // Contagem regressiva
+            for (let c = ent; c >= fin; c -= inc) {
+                mensagem.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        mensagem.innerHTML += `\u{1F3C1}`
+
+    }
 
 }
